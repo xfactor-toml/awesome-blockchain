3d65296e6dc7da39ce4644133be6264ca8c0278d
xfactor-toml <xfactor.toml@gmail.com>
2024-07-31 22:41:21 -0400

    feat(solana): update anchor version as v0.30.1 add readme

--- fullstack-solana/programs/counter/src/lib.rs
+++ fullstack-solana/programs/counter/src/lib.rs
@@ -1,12 +1,13 @@
 use anchor_lang::prelude::*;
 
-declare_id!("BCtae59k54QAvFniwLYRAGda7dAVpVk4gksDg8horPqN");
+declare_id!("2cP8NABvXKBvjCkXHN6chZZANRucemvnZo5vfmyG4JBH");
 
 #[program]
 pub mod counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
